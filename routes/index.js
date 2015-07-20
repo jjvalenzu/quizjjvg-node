6e4dbf5ebdad62ead587a192f79bb348f8448b71
jjvalenzu <trotajorge@googlemail.com>
2015-07-20 03:10:12 +0200

    Primera pregunta

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+var quizController = require('../controllers/quiz_controller');
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
+
+router.get('/quizes/question',  quizController.question);     // formulario sign un
+router.get('/quizes/answer',  quizController.answer);     // formulario sign un
 
 module.exports = router;
